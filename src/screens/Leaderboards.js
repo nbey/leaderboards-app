2300e2b768725438e77c0eb718587e2be6631f5d
Nafis Bey <nbey@jarv.us>
2018-05-01 11:48:56 -0400

    Implement sorting on the leaderboards grid

--- src/screens/Leaderboards.js
+++ src/screens/Leaderboards.js
@@ -17,13 +17,41 @@ export default class Leaderboards extends Component {
         super(props);
 
         this.state = {
-            players: []
+            players: [],
+            order: 'desc',
+            orderBy: 'wins'
         };
     }
 
     componentWillMount() {
         getPlayers()
-            .then(players => this.setState({players}));
+            .then(unsortedPlayers => this.sortPlayers(unsortedPlayers, this.state.orderBy, this.state.order))
+            .then((players) => {
+                debugger;
+                this.setState({
+                    players
+                });
+            });
+    }
+
+    onSortClick(orderBy, order = 'desc') {
+        if (this.state.orderBy === orderBy && this.state.order === 'desc') {
+            order = 'asc';
+        }
+
+        const players = this.sortPlayers(this.state.players, orderBy, order);
+
+        this.setState({
+            players,
+            order,
+            orderBy
+        });
+    }
+
+    sortPlayers(players, orderBy, order) {
+        return order === 'desc'
+            ? players.sort((a, b) => (b[orderBy] < a[orderBy] ? -1 : 1))
+            : players.sort((a, b) => (a[orderBy] < b[orderBy] ? -1 : 1));
     }
 
     render() {
@@ -35,13 +63,39 @@ export default class Leaderboards extends Component {
                             <TableCell>Name</TableCell>
                             <TableCell
                                 numeric={true}
+                                sortDirection={this.state.orderBy === 'wins' ? this.state.order : false}
                             >
                                 Wins
+                                <Tooltip
+                                    title="Sort"
+                                    placement={'bottom-end'}
+                                    enterDelay={150}
+                                    >
+                                    <TableSortLabel
+                                        active={this.state.orderBy === 'wins'}
+                                        direction={this.state.order}
+                                        onClick={() => this.onSortClick('wins', ...arguments)}
+                                    >
+                                    </TableSortLabel>
+                                </Tooltip>
                             </TableCell>
                             <TableCell
                                 numeric={true}
+                                sortDirection={this.state.orderBy === 'losses' ? this.state.order : false}
                             >
+                                <Tooltip
+                                    title="Sort"
+                                    placement={'bottom-end'}
+                                    enterDelay={150}
+                                    >
+                                    <TableSortLabel
+                                        active={this.state.orderBy === 'losses'}
+                                        direction={this.state.order}
+                                        onClick={() => this.onSortClick('losses', ...arguments)}
+                                    >
                                         Losses
+                                    </TableSortLabel>
+                                </Tooltip>
                             </TableCell>
                         </TableRow>
                     </TableHead>
