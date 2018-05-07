67e898b1a79c6c9e5fea66b7a9e22f2847842759
Nafis Bey <nbey@jarv.us>
2018-05-06 23:58:08 -0400

    Add AppBar and Drawer for navigation

--- src/App.js
+++ src/App.js
@@ -7,23 +7,76 @@ import {
 import './App.css';
 import NewGame from './screens/NewGame';
 import Leaderboards from './screens/Leaderboards';
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import {
+    AppBar,
+    Divider,
+    Drawer,
+    IconButton,
+    ListItem,
+    Toolbar,
+    Typography,
+} from 'material-ui';
+
+import {
+    Menu as MenuIcon,
+} from '@material-ui/icons';
+
+import { MuiThemeProvider, createMuiTheme, withStyles } from 'material-ui/styles';
 
 const theme = createMuiTheme();
 
 class App extends Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            drawerOpen: false
+        };
+
+        this.onClickHandler = this.onClickHandler.bind(this);
+    }
+
+    onClickHandler () {
+        this.setState({drawerOpen: false});
+    }
+
     render() {
         return (
             <MuiThemeProvider theme={theme}>
                 <HashRouter>
                     <div className="App">
-                        <header className="App-header">
-                            <h1 className="App-title">Pool Leaderboards</h1>
-                            <ul>
-                                <li><NavLink to="/new-game">New Game</NavLink></li>
-                                <li><NavLink to="/leaderboards">Leaderboards</NavLink></li>
-                            </ul>
+                        <header className="appHeader">
+                            <AppBar
+                                position={'static'}
+                            >
+                                <Toolbar>
+                                    <IconButton color={'inherit'} aria-label={'Menu'} onClick={() => this.setState({drawerOpen: !this.state.drawerOpen})}>
+                                        <MenuIcon />
+                                    </IconButton>
+                                    <Typography variant={'title'} className={'App-title'} style={{color: 'white'}}>Pool Leaderboards</Typography>
+                                </Toolbar>
+                            </AppBar>
+
                         </header>
+                        <Drawer
+                            variant="persistent"
+                            anchor={'left'}
+                            open={this.state.drawerOpen}
+
+                            PaperProps={{
+                                style:{
+                                    marginTop: 64 // TODO: replace w/ header height
+                                }
+                            }}
+                            >
+                            <ListItem>
+                                <NavLink className="nav-link" to="/new-game"><div onClick={this.onClickHandler}>New Game</div></NavLink>
+                            </ListItem>
+                            <Divider />
+                            <ListItem>
+                                <NavLink className="nav-link" to="/leaderboards"><div onClick={this.onClickHandler}>Leaderboards</div></NavLink>
+                            </ListItem>
+                        </Drawer>
                         <div className="App-content">
                             <Route path="/new-game" component={NewGame}/>
                             <Route path="/leaderboards" component={Leaderboards}/>
@@ -35,4 +88,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default withStyles(styles)(App);
